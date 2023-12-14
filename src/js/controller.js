d2a5553289dee541c9ac323dec65bca58ade3111
hiraksaharia <hiraksaharia4@gmail.com>
2023-12-14 10:34:41 +0530

    implemented list of ingredients using map() and join() method

--- src/js/controller.js
+++ src/js/controller.js
@@ -19,8 +19,8 @@ const timeout = function (s) {
 const showRecipe = async function () {
   try {
     const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc3e`
       // `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
     );
     // convert the result to json
     const data = await res.json();
@@ -58,14 +58,18 @@ const showRecipe = async function () {
       <svg class="recipe__info-icon">
         <use href="src/img/icons.svg#icon-clock"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+      <span class="recipe__info-data recipe__info-data--minutes">${
+        recipe.cookingTime
+      }</span>
       <span class="recipe__info-text">minutes</span>
     </div>
     <div class="recipe__info">
       <svg class="recipe__info-icon">
         <use href="src/img/icons.svg#icon-users"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+      <span class="recipe__info-data recipe__info-data--people">${
+        recipe.servings
+      }</span>
       <span class="recipe__info-text">servings</span>
 
       <div class="recipe__info-buttons">
@@ -97,35 +101,31 @@ const showRecipe = async function () {
   <div class="recipe__ingredients">
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
+    ${recipe.ingredients
+      .map(ing => {
+        return `
       <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="src/img/icons.svg#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">1000</div>
-        <div class="recipe__description">
-          <span class="recipe__unit">g</span>
-          pasta
-        </div>
-      </li>
-
-      <li class="recipe__ingredient">
-        <svg class="recipe__icon">
-          <use href="src/img/icons.svg#icon-check"></use>
-        </svg>
-        <div class="recipe__quantity">0.5</div>
-        <div class="recipe__description">
-          <span class="recipe__unit">cup</span>
-          ricotta cheese
-        </div>
-      </li>
-    </ul>
+      <svg class="recipe__icon">
+        <use href="src/img/icons.svg#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>
+      `;
+      })
+      .join('')}
   </div>
 
   <div class="recipe__directions">
     <h2 class="heading--2">How to cook it</h2>
     <p class="recipe__directions-text">
       This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+      <span class="recipe__publisher">${
+        recipe.publisher
+      }</span>. Please check out
       directions at their website.
     </p>
     <a
@@ -139,6 +139,7 @@ const showRecipe = async function () {
       </svg>
     </a>
   </div>`;
+    recipeContainer.innerHTML = '';
     recipeContainer.insertAdjacentHTML('afterbegin', markup);
   } catch (err) {
     alert(err);
